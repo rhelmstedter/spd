f9f4b18c3402d17f1a1d2d993562cf49d2f6c74b
Russell Helmstedter <rhelmstedter@gmail.com>
2022-06-25 10:22:31 -0700

    passes tests

--- spd.py
+++ spd.py
@@ -14,16 +14,28 @@ cli = typer.Typer()
 
 @cli.command()
 def write_csv(
+    location: Path = typer.Option(
+        CWD,
+        "--location",
+        "-L",
+        help="Directory file to read and write from if not cwd"
+    ),
     json_file: str = typer.Option(
-        "student_data.json", "--json", "-J", help="Json file to read"
+        "student_data.json",
+        "--json",
+        "-J",
+        help="Json file to read"
     ),
     csv_file: str = typer.Option(
-        "student_data.csv", "--csv", "-C", help="Csv file to write"
+        "student_data.csv",
+        "--csv",
+        "-C",
+        help="Csv file to write"
     ),
 ) -> None:
     """Writes a csv file based on the json data from PyBites"""
-    json_path = CWD / json_file
-    csv_path = CWD / csv_file
+    json_path = location.resolve() / json_file
+    csv_path = location.resolve() / csv_file
     if csv_path.is_file():
         print(f"[yellow]:warning: File already exists @ {csv_file}.")
         if not Confirm.ask("Do you want to overwrite the file?"):
@@ -40,6 +52,12 @@ def write_csv(
 
 @cli.command()
 def plot(
+    location: Path = typer.Option(
+        CWD,
+        "--location",
+        "-L",
+        help="Directory file to read csv if not cwd"
+    ),
     csv_file: str = typer.Option(
         "student_data.csv",
         "--csv",
@@ -55,21 +73,8 @@ def plot(
     ),
 ) -> None:
     """Plots average number of bites completed by class"""
-    csv_path = CWD / csv_file
-    df = pd.read_csv(csv_path)
-    data = (
-        df.where(df.class_ != "DEA")
-        .assign(
-            total_completed=lambda x:
-            x.newbie_completed
-            + x.intro_completed
-            + x.regular_completed
-        )
-        .groupby(["class_"])
-        .mean()
-        .reset_index()
-        .round(0)
-    )
+    csv_path = location.resolve() / csv_file
+    data = clean_data(csv_path)
     if sort:
         data = data.sort_values("total_completed", ascending=True).reset_index()
 
@@ -79,6 +84,22 @@ def plot(
     plt.title("Average Bites completed by class")
     plt.xlim(0, 33)
     plt.show()
+
+
+def clean_data(csv_path):
+    df = pd.read_csv(csv_path)
+    return (
+        df.where(df.class_ != "DEA")
+        .assign(
+            total_completed=lambda x: x.newbie_completed
+            + x.intro_completed
+            + x.regular_completed
+        )
+        .groupby(["class_"])
+        .mean()
+        .reset_index()
+        .round(1)
+    )
 
 
 if __name__ == "__main__":
---
+++ test_spd.py
@@ -0,0 +1,67 @@
+import pytest
+import os
+import json
+from pathlib import Path
+
+from spd import write_csv, plot, clean_data
+
+TEST_JSON = [
+    {
+        "name": "nottoday satan",
+        "email": "nottoday.satan@pibites.org",
+        "class_": "DEA",
+        "profile_url": "https://codechalleng.es/profiles/nottodaysatan",
+        "newbie_completed": 3,
+        "intro_completed": 1,
+        "regular_completed": 3,
+        "certificates": ""
+    },
+    {
+        "name": "nottoday satan",
+        "email": "nottoday.satan@pibites.org",
+        "class_": "period 2",
+        "profile_url": "https://codechalleng.es/profiles/nottodaysatan",
+        "newbie_completed": 2,
+        "intro_completed": 8,
+        "regular_completed": 1,
+        "certificates": ""
+    }
+]
+
+TEST_CSV = """
+name,email,class_,profile_url,newbie_completed,intro_completed,regular_completed,certificates
+nottoday satan,nottoday.satan@pibites.org,DEA,https://codechalleng.es/profiles/nottodaysatan,3,1,3,
+nottoday satan,nottoday.satan@pibites.org,period 2,https://codechalleng.es/profiles/nottodaysatan,2,8,1,
+"""
+
+
+def test_csv_writer(tmp_path):
+    d = tmp_path / "testing_dir"
+    d.mkdir()
+    json_file = 'test_data.json'
+    with open(d / json_file, 'w') as f:
+        json.dump(TEST_JSON, f)
+    write_csv(location=d, json_file=json_file, csv_file='testing.csv')
+    assert (d / 'testing.csv').is_file()
+
+
+def test_plot(tmp_path, capfd):
+    d = tmp_path / "testing_dir"
+    d.mkdir()
+    csv_file = 'test_csv.csv'
+    with open(d / csv_file, 'w') as f:
+        f.write(TEST_CSV.strip('\n'))
+    plot(location=d, csv_file=csv_file)
+    output = capfd.readouterr()[0].strip()
+    assert "period 2" in output
+
+
+def test_clean_data(tmp_path):
+    d = tmp_path / "testing_dir"
+    d.mkdir()
+    csv_file = 'test_csv.csv'
+    with open(d / csv_file, 'w') as f:
+        f.write(TEST_CSV.strip('\n'))
+    testing_data = clean_data(d / csv_file)
+    assert len(testing_data) == 1 # filters out the DEA
+    assert testing_data.iloc[0]['total_completed'] == 11 # does the correct assinment for df.total_completed
