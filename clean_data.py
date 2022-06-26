33f6f72fe4623400fedc88db92e2f2c755113e56
Russell Helmstedter <rhelmstedter@gmail.com>
2022-06-26 09:10:34 -0700

    add a clean_data script; desensitized data to post; updated .gitingore

---
+++ clean_data.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import json
+from rich import print
+
+JSON_PATH = Path.cwd() / 'student_data.json'
+CLEANED_PATH = Path.cwd() / 'cleaned_data.json'
+
+with open(JSON_PATH) as f:
+    data = json.load(f)
+
+cleaned_data = []
+
+for i, student in enumerate(data[2:], 1):
+    student["name"] = f"student{i}"
+    student["email"] = f"student{i}@pybites.org"
+    student["profile_url"] = f"https://codechalleng.es/profiles/student{i}"
+    student["certificates"] = ""
+    cleaned_data.append(student)
+
+
+with open(CLEANED_PATH, 'w') as f:
+    json.dump(cleaned_data, f)
